f9cf61521b257177f95916d9058a7f043457bc2f
mmctongshi <110273155+mmctongshi@users.noreply.github.com>
2022-07-30 13:41:19 +0800

    Update course.js

--- course.js
+++ course.js
@@ -78,4 +78,4 @@ function g09() {window.location.href="n07_yuyan/g09.html";}
 function g10() {window.location.href="n07_yuyan/g10.html";} 
 function g11() {window.location.href="n07_yuyan/g11.html";} 
 function h01() {window.location.href="n08_other/h01.html";} 
-function tian() {window.location.href="n01_tian";} 
+function n01_tian() {window.location.href="n01_tian/index.html";} 
